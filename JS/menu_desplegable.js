2ff282d67ddd6615fa2aeb15e6b350f245bbfea5
Santiago Aguilar <santiagaguilardecano@gmail.com>
2026-07-11 20:59:35 -0300

    Create menu_desplegable.js

---
+++ JS/menu_desplegable.js
@@ -0,0 +1,6 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
